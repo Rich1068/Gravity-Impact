663cb5a1e75f0e38cc5a83ba7b3f84516dd9c464
Windows 10 Pro 64Bit <Windows 10 Pro 64Bit@DESKTOP-POTNS7O>
2022-11-11 14:41:02 +0800

    Add project files.

---
+++ Gravity Impact/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Gravity_Impact
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblhighScore = new System.Windows.Forms.Label();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Gravity_Impact.Properties.Resources.download;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(-24, -1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(845, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::Gravity_Impact.Properties.Resources.download;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(-15, 377);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(845, 50);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblScore.ForeColor = System.Drawing.Color.Transparent;
+            this.lblScore.Location = new System.Drawing.Point(12, 18);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(57, 21);
+            this.lblScore.TabIndex = 2;
+            this.lblScore.Text = "label1";
+            // 
+            // lblhighScore
+            // 
+            this.lblhighScore.AutoSize = true;
+            this.lblhighScore.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblhighScore.ForeColor = System.Drawing.Color.Transparent;
+            this.lblhighScore.Location = new System.Drawing.Point(12, 395);
+            this.lblhighScore.Name = "lblhighScore";
+            this.lblhighScore.Size = new System.Drawing.Size(57, 21);
+            this.lblhighScore.TabIndex = 3;
+            this.lblhighScore.Text = "label2";
+            this.lblhighScore.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // player
+            // 
+            this.player.Image = global::Gravity_Impact.Properties.Resources.catto;
+            this.player.Location = new System.Drawing.Point(101, 303);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(67, 80);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 4;
+            this.player.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Gravity_Impact.Properties.Resources.chinese_tom;
+            this.pictureBox4.Location = new System.Drawing.Point(424, 39);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(67, 80);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "obstacle";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Gravity_Impact.Properties.Resources.chinese_tom;
+            this.pictureBox5.Location = new System.Drawing.Point(633, 303);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(67, 80);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Tag = "obstacle";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Gravity_Impact.Properties.Resources.bef05f3bc48d34bd386b521e970dd801_1822064499909099042;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(812, 425);
+            this.Controls.Add(this.lblhighScore);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private System.Windows.Forms.Timer gameTimer;
+        private Label lblScore;
+        private Label lblhighScore;
+        private PictureBox player;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+    }
+}
---
+++ Gravity Impact/Form1.cs
@@ -0,0 +1,123 @@
+using static System.Formats.Asn1.AsnWriter;
+using System.Numerics;
+using System;
+
+namespace Gravity_Impact
+{
+    public partial class Form1 : Form
+    {
+        int gravity;
+        int gravityValue = 8;
+        int obstacleSpeed = 10;
+        int score = 0;
+        int highScore = 0;
+        bool gameStart = false;
+        Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                if (player.Top == 303)
+                {
+                    player.Top -= 10;
+                    gravity = -gravityValue;
+                }
+                else if (player.Top == 39)
+                {
+                    player.Top += 10;
+                    gravity = gravityValue;
+                }
+            }
+
+            if (e.KeyCode == Keys.Enter && gameStart == false)
+            {
+                RestartGame();
+            }
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+            lblScore.Text = "Score: " + score;
+            lblhighScore.Text = "High Score: " + highScore;
+            player.Top += gravity;
+
+            // when the player land on the platforms. 
+            if (player.Top > 303)
+            {
+                gravity = 0;
+                player.Top = 303;
+                player.Image = Properties.Resources.Dead;
+            }
+            else if (player.Top < 39)
+            {
+                gravity = 0;
+                player.Top = 39;
+                player.Image = Properties.Resources.catto;
+            }
+            // move the obstacles
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Tag as string == "obstacle")
+                {
+                    x.Left -= obstacleSpeed;
+
+                    if (x.Left < -100)
+                    {
+                        x.Left = random.Next(1200, 3000);
+                        score += 1;
+                    }
+
+                    if (x.Bounds.IntersectsWith(player.Bounds))
+                    {
+                        gameTimer.Stop();
+                        lblScore.Text += " Game Over!! Press Enter to Restart.";
+                        gameStart = false;
+                        // set the high score 
+                        if (score > highScore)
+                        {
+                            highScore = score;
+                        }
+                    }
+                }
+            }
+        }
+        private void RestartGame()
+        {
+            lblScore.Parent = pictureBox1;
+            lblhighScore.Parent = pictureBox2;
+            lblhighScore.Top = 0;
+            player.Location = new Point(98, 203);
+            player.Image = Properties.Resources.catto;
+            score = 0;
+            gravityValue = 8;
+            gravity = gravityValue;
+            obstacleSpeed = 10;
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x.Tag as string == "obstacle")
+                {
+                    x.Left = random.Next(1200, 3000);
+                }
+            }
+
+            gameTimer.Start();
+
+        }
+    }
+}
